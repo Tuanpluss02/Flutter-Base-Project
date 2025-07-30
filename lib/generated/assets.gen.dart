439f5f27b6bfc2fa11081a7c5e9a1d98641d3d62
tuan.dn <dongoctuan2002sp@gmail.com>
2025-07-30 11:01:09 +0700

    feat(assets): generate asset classes for fonts, images, lotties, rives, svgs, and translations
    - Add classes for managing various asset types including fonts, images, lotties, rives, svgs, and translations.
    - Implement methods to retrieve asset paths and lists for easy access.
    - Enhance asset management capabilities in the application.

--- lib/generated/assets.gen.dart
+++ lib/generated/assets.gen.dart
@@ -7,6 +7,13 @@
 // ignore_for_file: type=lint
 // ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
 
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart' as _svg;
+import 'package:lottie/lottie.dart' as _lottie;
+import 'package:rive/rive.dart' as _rive;
+import 'package:vector_graphics/vector_graphics.dart' as _vg;
+
 class $AssetsEnvGen {
   const $AssetsEnvGen();
 
@@ -23,8 +30,430 @@ class $AssetsEnvGen {
   List<String> get values => [aEnv, aEnvDev, aEnvProduction];
 }
 
+class $AssetsFontsGen {
+  const $AssetsFontsGen();
+
+  /// File path: assets/fonts/SF-Pro-Display-Black.otf
+  String get sFProDisplayBlack => 'assets/fonts/SF-Pro-Display-Black.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-BlackItalic.otf
+  String get sFProDisplayBlackItalic =>
+      'assets/fonts/SF-Pro-Display-BlackItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Bold.otf
+  String get sFProDisplayBold => 'assets/fonts/SF-Pro-Display-Bold.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-BoldItalic.otf
+  String get sFProDisplayBoldItalic =>
+      'assets/fonts/SF-Pro-Display-BoldItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Heavy.otf
+  String get sFProDisplayHeavy => 'assets/fonts/SF-Pro-Display-Heavy.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-HeavyItalic.otf
+  String get sFProDisplayHeavyItalic =>
+      'assets/fonts/SF-Pro-Display-HeavyItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Light.otf
+  String get sFProDisplayLight => 'assets/fonts/SF-Pro-Display-Light.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-LightItalic.otf
+  String get sFProDisplayLightItalic =>
+      'assets/fonts/SF-Pro-Display-LightItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Medium.otf
+  String get sFProDisplayMedium => 'assets/fonts/SF-Pro-Display-Medium.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-MediumItalic.otf
+  String get sFProDisplayMediumItalic =>
+      'assets/fonts/SF-Pro-Display-MediumItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Regular.otf
+  String get sFProDisplayRegular => 'assets/fonts/SF-Pro-Display-Regular.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-RegularItalic.otf
+  String get sFProDisplayRegularItalic =>
+      'assets/fonts/SF-Pro-Display-RegularItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Semibold.otf
+  String get sFProDisplaySemibold => 'assets/fonts/SF-Pro-Display-Semibold.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-SemiboldItalic.otf
+  String get sFProDisplaySemiboldItalic =>
+      'assets/fonts/SF-Pro-Display-SemiboldItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Thin.otf
+  String get sFProDisplayThin => 'assets/fonts/SF-Pro-Display-Thin.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-ThinItalic.otf
+  String get sFProDisplayThinItalic =>
+      'assets/fonts/SF-Pro-Display-ThinItalic.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-Ultralight.otf
+  String get sFProDisplayUltralight =>
+      'assets/fonts/SF-Pro-Display-Ultralight.otf';
+
+  /// File path: assets/fonts/SF-Pro-Display-UltralightItalic.otf
+  String get sFProDisplayUltralightItalic =>
+      'assets/fonts/SF-Pro-Display-UltralightItalic.otf';
+
+  /// List of all assets
+  List<String> get values => [
+        sFProDisplayBlack,
+        sFProDisplayBlackItalic,
+        sFProDisplayBold,
+        sFProDisplayBoldItalic,
+        sFProDisplayHeavy,
+        sFProDisplayHeavyItalic,
+        sFProDisplayLight,
+        sFProDisplayLightItalic,
+        sFProDisplayMedium,
+        sFProDisplayMediumItalic,
+        sFProDisplayRegular,
+        sFProDisplayRegularItalic,
+        sFProDisplaySemibold,
+        sFProDisplaySemiboldItalic,
+        sFProDisplayThin,
+        sFProDisplayThinItalic,
+        sFProDisplayUltralight,
+        sFProDisplayUltralightItalic
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/cml.png
+  AssetGenImage get cml => const AssetGenImage('assets/images/cml.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [cml];
+}
+
+class $AssetsLottiesGen {
+  const $AssetsLottiesGen();
+
+  /// File path: assets/lotties/lottie_test.json
+  LottieGenImage get lottieTest =>
+      const LottieGenImage('assets/lotties/lottie_test.json');
+
+  /// List of all assets
+  List<LottieGenImage> get values => [lottieTest];
+}
+
+class $AssetsRivesGen {
+  const $AssetsRivesGen();
+
+  /// File path: assets/rives/rive_test.riv
+  RiveGenImage get riveTest => const RiveGenImage('assets/rives/rive_test.riv');
+
+  /// List of all assets
+  List<RiveGenImage> get values => [riveTest];
+}
+
+class $AssetsSvgsGen {
+  const $AssetsSvgsGen();
+
+  /// File path: assets/svgs/menu.svg
+  SvgGenImage get menu => const SvgGenImage('assets/svgs/menu.svg');
+
+  /// List of all assets
+  List<SvgGenImage> get values => [menu];
+}
+
+class $AssetsTranslationsGen {
+  const $AssetsTranslationsGen();
+
+  /// File path: assets/translations/en.json
+  String get en => 'assets/translations/en.json';
+
+  /// File path: assets/translations/vi.json
+  String get vi => 'assets/translations/vi.json';
+
+  /// List of all assets
+  List<String> get values => [en, vi];
+}
+
 class Assets {
   const Assets._();
 
   static const $AssetsEnvGen env = $AssetsEnvGen();
+  static const $AssetsFontsGen fonts = $AssetsFontsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+  static const $AssetsLottiesGen lotties = $AssetsLottiesGen();
+  static const $AssetsRivesGen rives = $AssetsRivesGen();
+  static const $AssetsSvgsGen svgs = $AssetsSvgsGen();
+  static const $AssetsTranslationsGen translations = $AssetsTranslationsGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+
+  final Size? size;
+  final Set<String> flavors;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = true,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.medium,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class SvgGenImage {
+  const SvgGenImage(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  }) : _isVecFormat = false;
+
+  const SvgGenImage.vec(
+    this._assetName, {
+    this.size,
+    this.flavors = const {},
+  }) : _isVecFormat = true;
+
+  final String _assetName;
+  final Size? size;
+  final Set<String> flavors;
+  final bool _isVecFormat;
+
+  _svg.SvgPicture svg({
+    Key? key,
+    bool matchTextDirection = false,
+    AssetBundle? bundle,
+    String? package,
+    double? width,
+    double? height,
+    BoxFit fit = BoxFit.contain,
+    AlignmentGeometry alignment = Alignment.center,
+    bool allowDrawingOutsideViewBox = false,
+    WidgetBuilder? placeholderBuilder,
+    String? semanticsLabel,
+    bool excludeFromSemantics = false,
+    _svg.SvgTheme? theme,
+    ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
+    @deprecated Color? color,
+    @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
+    @deprecated bool cacheColorFilter = false,
+  }) {
+    final _svg.BytesLoader loader;
+    if (_isVecFormat) {
+      loader = _vg.AssetBytesLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+      );
+    } else {
+      loader = _svg.SvgAssetLoader(
+        _assetName,
+        assetBundle: bundle,
+        packageName: package,
+        theme: theme,
+      );
+    }
+    return _svg.SvgPicture(
+      loader,
+      key: key,
+      matchTextDirection: matchTextDirection,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      allowDrawingOutsideViewBox: allowDrawingOutsideViewBox,
+      placeholderBuilder: placeholderBuilder,
+      semanticsLabel: semanticsLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      colorFilter: colorFilter ??
+          (color == null ? null : ColorFilter.mode(color, colorBlendMode)),
+      clipBehavior: clipBehavior,
+      cacheColorFilter: cacheColorFilter,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class RiveGenImage {
+  const RiveGenImage(
+    this._assetName, {
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+  final Set<String> flavors;
+
+  _rive.RiveAnimation rive({
+    String? artboard,
+    List<String> animations = const [],
+    List<String> stateMachines = const [],
+    BoxFit? fit,
+    Alignment? alignment,
+    Widget? placeHolder,
+    bool antialiasing = true,
+    bool useArtboardSize = false,
+    List<_rive.RiveAnimationController> controllers = const [],
+    _rive.OnInitCallback? onInit,
+  }) {
+    return _rive.RiveAnimation.asset(
+      _assetName,
+      artboard: artboard,
+      animations: animations,
+      stateMachines: stateMachines,
+      fit: fit,
+      alignment: alignment,
+      placeHolder: placeHolder,
+      antialiasing: antialiasing,
+      useArtboardSize: useArtboardSize,
+      controllers: controllers,
+      onInit: onInit,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
+
+class LottieGenImage {
+  const LottieGenImage(
+    this._assetName, {
+    this.flavors = const {},
+  });
+
+  final String _assetName;
+  final Set<String> flavors;
+
+  _lottie.LottieBuilder lottie({
+    Animation<double>? controller,
+    bool? animate,
+    _lottie.FrameRate? frameRate,
+    bool? repeat,
+    bool? reverse,
+    _lottie.LottieDelegates? delegates,
+    _lottie.LottieOptions? options,
+    void Function(_lottie.LottieComposition)? onLoaded,
+    _lottie.LottieImageProviderFactory? imageProviderFactory,
+    Key? key,
+    AssetBundle? bundle,
+    Widget Function(
+      BuildContext,
+      Widget,
+      _lottie.LottieComposition?,
+    )? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    double? width,
+    double? height,
+    BoxFit? fit,
+    AlignmentGeometry? alignment,
+    String? package,
+    bool? addRepaintBoundary,
+    FilterQuality? filterQuality,
+    void Function(String)? onWarning,
+    _lottie.LottieDecoder? decoder,
+    _lottie.RenderCache? renderCache,
+    bool? backgroundLoading,
+  }) {
+    return _lottie.Lottie.asset(
+      _assetName,
+      controller: controller,
+      animate: animate,
+      frameRate: frameRate,
+      repeat: repeat,
+      reverse: reverse,
+      delegates: delegates,
+      options: options,
+      onLoaded: onLoaded,
+      imageProviderFactory: imageProviderFactory,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      width: width,
+      height: height,
+      fit: fit,
+      alignment: alignment,
+      package: package,
+      addRepaintBoundary: addRepaintBoundary,
+      filterQuality: filterQuality,
+      onWarning: onWarning,
+      decoder: decoder,
+      renderCache: renderCache,
+      backgroundLoading: backgroundLoading,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
 }
